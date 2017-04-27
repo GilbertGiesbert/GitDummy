f0821276994e82dc7ae58a0277064f5ef67a0248
Joern Sattler <joern.sattler.ext@init.de>
2017-04-27 19:05:40 +0200

    added git commit and pull

--- src/main/java/com/joern/dummies/gitdummy/App.java
+++ src/main/java/com/joern/dummies/gitdummy/App.java
@@ -1,18 +1,9 @@
 package com.joern.dummies.gitdummy;
 
 import org.apache.commons.lang3.StringUtils;
-import org.eclipse.jgit.api.CloneCommand;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.PullCommand;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.File;
-import java.io.IOException;
 
 /**
  * Created by jsattler on 27.04.2017.
@@ -21,58 +12,34 @@ public class App {
 
     private static final Logger l = LoggerFactory.getLogger(App.class);
 
-    public static void main(String... args ){
-
-        new App().gitPull();
-    }
-
-    public void gitClone(String... args){
+    public static void main(String... args){
 
         String gitUser = args[0];
         String gitPswd = args[1];
+
+        new App().useGit(gitUser, gitPswd);
+    }
+
+    public void useGit(String gitUser, String gitPswd){
+
         String gitRepoUrl = PropertyReader.readProperty("git.remote.repoUrl");
         String gitWorkDir = PropertyReader.readProperty("git.local.workDir");
         String gitBranch = PropertyReader.readProperty("git.branch");
 
         if(StringUtils.isBlank(gitRepoUrl) || StringUtils.isBlank(gitWorkDir) || StringUtils.isBlank(gitBranch)){
-            l.error("Failed to start git client due to invalid config");
+            l.error("Failed to use git, missing some config");
             return;
         }
 
-        try {
-            CloneCommand cc = Git.cloneRepository()
-                    .setURI( gitRepoUrl )
-                    .setDirectory( new File(gitWorkDir) )
-                    .setBranch(gitBranch);
+        // GitUtil.clone(gitRepoUrl, gitWorkDir, gitBranch);
 
-            if(StringUtils.isNotBlank(gitUser) && StringUtils.isNotBlank(gitPswd)){
-                cc.setCredentialsProvider( new UsernamePasswordCredentialsProvider( gitUser, gitPswd ) );
-            }
-            Git git =  cc.call();
+        Repository localRepo = GitUtil.getLocalRepository();
+        if(localRepo != null){
 
-        } catch (GitAPIException e) {
-            l.error("Failed to git clone", e);
-        }
-    }
+            // GitUtil.pull(localRepo);
 
-    public void gitPull(){
-
-        String gitWorkDir = PropertyReader.readProperty("git.local.workDir");
-
-        Repository localRepo = null;
-        try {
-            localRepo = new FileRepository(gitWorkDir + "/.git");
-        } catch (IOException e) {
-            l.error("Failed to init local repo", e);
-            return;
-        }
-
-        Git git = new Git(localRepo);
-        PullCommand pc = git.pull();
-        try {
-            pc.call();
-        } catch (GitAPIException e) {
-            l.error("Failed to git pull", e);
+            GitUtil.commit(localRepo);
+            GitUtil.push(localRepo, gitUser, gitPswd);
         }
     }
 }
---
+++ src/main/java/com/joern/dummies/gitdummy/GitUtil.java
@@ -0,0 +1,113 @@
+package com.joern.dummies.gitdummy;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.CommitCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullCommand;
+import org.eclipse.jgit.api.PushCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by jsattler on 27.04.2017.
+ */
+public class GitUtil {
+
+    private static final Logger l = LoggerFactory.getLogger(GitUtil.class);
+
+    public static void clone(String gitRepoUrl, String gitWorkDir, String gitBranch){
+
+        try {
+            CloneCommand clone = Git.cloneRepository()
+                    .setURI( gitRepoUrl )
+                    .setDirectory( new File(gitWorkDir) )
+                    .setBranch(gitBranch);
+            clone.call();
+
+        } catch (GitAPIException e) {
+            l.error("Failed to git clone", e);
+        }
+    }
+
+    public static void pull(Repository localRepo){
+
+        if(localRepo == null){
+            l.error("Failed to git pull, missing local repo.");
+            return;
+        }
+
+        Git git = new Git(localRepo);
+        PullCommand pull = git.pull();
+        try {
+            pull.call();
+        } catch (GitAPIException e) {
+            l.error("Failed to git pull", e);
+        }
+    }
+
+    public static void push(Repository localRepo, String gitUser, String gitPswd){
+
+        if(localRepo == null){
+            l.error("Failed to git push, missing local repo.");
+            return;
+        }
+
+        Git git = new Git(localRepo);
+        PushCommand push = git.push();
+        if(StringUtils.isNotBlank(gitUser) && StringUtils.isNotBlank(gitPswd)){
+            push.setCredentialsProvider( new UsernamePasswordCredentialsProvider( gitUser, gitPswd ) );
+        }
+        try {
+            push.call();
+        } catch (GitAPIException e) {
+            l.error("Failed to git pull", e);
+        }
+    }
+
+
+    public static void commit(Repository localRepo){
+
+        if(localRepo == null){
+            l.error("Failed to git commit, missing local repo.");
+            return;
+        }
+
+        Git git = new Git(localRepo);
+        String addAll = ".";
+
+        try {
+            git.add().addFilepattern(addAll).call();
+        } catch (GitAPIException e) {
+            l.error("Failed to git commit, can't add files to stage", e);
+        }
+
+        CommitCommand commit = git.commit();
+        commit.setMessage("java commit at "+TimeStamp.stamp());
+        try {
+            commit.call();
+        } catch (GitAPIException e) {
+            l.error("Failed to git commit", e);
+        }
+    }
+
+    public static Repository getLocalRepository(){
+
+        String gitWorkDir = PropertyReader.readProperty("git.local.workDir");
+
+        Repository localRepo = null;
+        try {
+            localRepo = new FileRepository(gitWorkDir + "/.git");
+        } catch (IOException e) {
+            l.error("Failed to access local repo", e);
+        }
+        return localRepo;
+    }
+}
