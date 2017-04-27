e6c676bb4a9b21316bd2393b739af93fed66674a
Joern Sattler <joern.sattler.ext@init.de>
2017-04-27 17:43:05 +0200

    added time stamp util

---
+++ src/main/java/com/joern/dummies/gitdummy/TimeStamp.java
@@ -0,0 +1,23 @@
+package com.joern.dummies.gitdummy;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by jsattler on 27.04.2017.
+ */
+public class TimeStamp {
+
+    public static String stamp(){
+
+        Date date = new Date();
+        String dateFormat = "yyyy.MM.dd - hh:mm:ss";
+        return stamp(date, dateFormat);
+    }
+
+    public static String stamp(Date date, String dateFormat){
+
+        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+        return sdf.format(date);
+    }
+}
