5b6e03bf7cb122ce9eb850691f9850b973470641
Joern Sattler <joern.sattler.ext@init.de>
2017-05-15 12:46:47 +0200

    more git use cases

--- src/main/java/com/joern/dummies/gitdummy/App.java
+++ src/main/java/com/joern/dummies/gitdummy/App.java
@@ -2,8 +2,13 @@ package com.joern.dummies.gitdummy;
 
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ResetCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Date;
 
 /**
  * Created by jsattler on 27.04.2017.
@@ -24,7 +29,7 @@ public class App {
         String localRepoPath = PropertyReader.readProperty("git.local.repoPath");
 
         App app = new App();
-        app.gitExample(user, password, remoteRepoUrl, localRepoPath);
+        app.gitExample2(user, password, remoteRepoUrl, localRepoPath);
     }
 
     public void gitExample(String user, String password, String remoteRepoUrl, String localRepoPath){
@@ -51,6 +56,8 @@ public class App {
 
             // GitUtil.merge("worker", git);
 
+            // GitUtil.reset("5d18cc6b23e1bd14426b909c3357631f7dbc74c5", ResetCommand.ResetType.HARD, git);
+
         }
     }
 
@@ -69,7 +76,7 @@ public class App {
             String featureBranch = "myFeature";
             String developBranch = "develop";
             String configFile = localRepoPath+"/src/main/resources/puppyprops/config.properties";
-
+            String devConfig = localRepoPath+"/src/main/resources/puppyprops/devConfig.properties";
 
 
             // make feature changes and push
@@ -84,6 +91,7 @@ public class App {
             // make develop changes and push
             GitUtil.checkout(developBranch, git);
             FileEdit.editFileContent(configFile, developBranch+" branch was here", true);
+            FileEdit.editFileContent(devConfig, developBranch+" branch was here", true);
             GitUtil.stage(GitUtil.FILE_PATTERN_STAGE_ALL, git);
             GitUtil.commit(developBranch+" changes", git);
             GitUtil.push(git, user, password);
@@ -94,6 +102,64 @@ public class App {
             GitUtil.push(git, user, password);
 
         }
+    }
+
+
+    public void gitExample2(String user, String password, String remoteRepoUrl, String localRepoPath) {
+
+
+        Git git = GitUtil.getGit(localRepoPath);
+        if (git != null) {
+
+            String featureBranch = "myFeature_"+TimeStamp.stamp(new Date(), "sss");
+            String developBranch = "develop";
+            String relPathResources = "src/main/resources";
+            String devConfig = localRepoPath+"/"+relPathResources+"/config/devConfig.properties";
+            String puppyConfig = localRepoPath+"/"+relPathResources+"/config/puppyConfig.properties";
+            String databaseConfig = localRepoPath+"/"+relPathResources+"/database/database.properties";
+
+            String rollbackRef = null;
+            try {
+                rollbackRef = git.getRepository().findRef(developBranch).getObjectId().getName();
+            } catch (IOException e) {
+                l.error("Failed to set rollback point");
+            }
+
+            // make feature changes and commit
+            GitUtil.branch(featureBranch, git);
+            GitUtil.checkout(featureBranch, git);
+            // FileEdit.editFileContent(devConfig, featureBranch+" branch was here", false);
+            FileEdit.editFileContent(puppyConfig, featureBranch+" branch was here", false);
+            // FileEdit.editFileContent(databaseConfig, featureBranch+" branch was here", false);
+            //GitUtil.stage(relPathResources+"/config/puppyConfig.properties", git);
+            GitUtil.stage(GitUtil.FILE_PATTERN_STAGE_ALL, git);
+            GitUtil.commit(featureBranch+" changes", git);
+
+            // make develop changes and commit
+            GitUtil.checkout(developBranch, git);
+            FileEdit.editFileContent(devConfig, developBranch+" branch was here", false);
+            // FileEdit.editFileContent(puppyConfig, developBranch+" branch was here", false);
+            FileEdit.editFileContent(databaseConfig, developBranch+" branch was here", false);
+            GitUtil.stage(GitUtil.FILE_PATTERN_STAGE_ALL, git);
+            GitUtil.commit(developBranch+" changes", git);
+            // GitUtil.push(git, user, password);
+
+
+            // merge feature into develop
+            boolean mergeSuccess = GitUtil.merge(featureBranch, null, git);
+            if(mergeSuccess){
+
+                // and push
+                GitUtil.push(git, user, password);
+            }else{
+                // reset develop
+                if(StringUtils.isNotBlank(rollbackRef)){
+                    GitUtil.reset(rollbackRef, ResetCommand.ResetType.HARD, git);
+                }
+            }
+            GitUtil.deleteLocalBranch(featureBranch, true, git);
+        }
+
     }
 
 }
--- src/main/java/com/joern/dummies/gitdummy/GitUtil.java
+++ src/main/java/com/joern/dummies/gitdummy/GitUtil.java
@@ -1,23 +1,36 @@
 package com.joern.dummies.gitdummy;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.CommitCommand;
+import org.eclipse.jgit.api.FetchCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.MergeCommand;
+import org.eclipse.jgit.api.MergeResult;
 import org.eclipse.jgit.api.PullCommand;
+import org.eclipse.jgit.api.PullResult;
 import org.eclipse.jgit.api.PushCommand;
+import org.eclipse.jgit.api.RebaseResult;
+import org.eclipse.jgit.api.ResetCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.BranchConfig;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.merge.MergeStrategy;
+import org.eclipse.jgit.merge.Merger;
+import org.eclipse.jgit.transport.FetchResult;
+import org.eclipse.jgit.transport.PushResult;
+import org.eclipse.jgit.transport.RefSpec;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by jsattler on 27.04.2017.
@@ -54,6 +67,57 @@ public class GitUtil {
         PullCommand pull = git.pull();
         try {
             pull.call();
+        } catch (GitAPIException e) {
+            l.error("Failed to git pull", e);
+            success = false;
+        }
+        return success;
+    }
+
+    /*
+        Note: don't know how to implement
+     */
+    static boolean pullAndMerge(Git git){
+
+        Merger ourMerger = MergeStrategy.OURS.newMerger(git.getRepository());
+        Merger theirMerger = MergeStrategy.THEIRS.newMerger(git.getRepository());
+
+        boolean success = true;
+
+        FetchCommand fetch = git.fetch();
+
+        // PullCommand pull = git.pull();
+        // PullCommand pull = git.pull().setRebase(BranchConfig.BranchRebaseMode.NONE).setStrategy(MergeStrategy.RECURSIVE);
+        // PullCommand pull = git.pull().setStrategy(MergeStrategy.RESOLVE);
+
+        try {
+
+            FetchResult fetchResult = fetch.call();
+            Ref ref = fetchResult.getAdvertisedRef("develop");
+            MergeCommand merge = git.merge().include(ref);
+
+            MergeResult mergeResult = merge.call();
+            Map<String, int[][]>  conflictsMap = mergeResult.getConflicts();
+
+            for(String s: conflictsMap.keySet()){
+
+                int[][] bla = conflictsMap.get(s);
+
+            }
+
+
+            // PullResult pullResult = pull.call();
+            // MergeResult mergeResult = pullResult.getMergeResult();
+
+
+
+
+
+
+
+
+
+
         } catch (GitAPIException e) {
             l.error("Failed to git pull", e);
             success = false;
@@ -164,7 +228,38 @@ public class GitUtil {
             if(StringUtils.isNotBlank(mergeMessage)){
                 merge.setMessage(mergeMessage);
             }
-            merge.call();
+            MergeResult mergeResult = merge.call();
+
+            success = mergeResult.getConflicts() == null;
+
+        } catch (IOException | GitAPIException e) {
+            l.error("Failed to merge branch "+theirBranchName + " into " + currentBranch, e);
+            success = false;
+        }
+        return success;
+    }
+
+    public static boolean merge(String theirBranchName, String mergeMessage, Git git){
+
+        boolean success = true;
+        String currentBranch = null;
+
+        try {
+
+            Repository repository = git.getRepository();
+            currentBranch = repository.getBranch();
+
+            Ref ref = repository.findRef(theirBranchName);
+            MergeCommand merge = git.merge().include(ref);
+            if(StringUtils.isNotBlank(mergeMessage)){
+                merge.setMessage(mergeMessage);
+            }
+
+
+            MergeResult mergeResult = merge.call();
+            Map<String, int[][]> conflictsMap = mergeResult.getConflicts();
+
+            success = conflictsMap == null;
 
         } catch (IOException | GitAPIException e) {
             l.error("Failed to merge branch "+theirBranchName + " into " + currentBranch, e);
@@ -174,10 +269,47 @@ public class GitUtil {
     }
 
 
-    // TODO
-    public static boolean deleteBranch(String branchName, Git git){
+    // TODO: test
+    public static boolean deleteLocalBranch(String branchName, boolean forceDelete, Git git){
 
-        l.error("not implemented: deleteBranch()");
-        return false;
+        boolean success = true;
+
+        try {
+            git.branchDelete().setForce(forceDelete).setBranchNames("refs/heads/"+branchName).call();
+        } catch (GitAPIException e) {
+            l.error("Failed to delete local branch "+branchName, e);
+            success = false;
+        }
+
+        return success;
+    }
+
+    // TODO: test
+    public static boolean deleteRemoteBranch(String branchName, Git git){
+
+        boolean success = true;
+        String remoteServerName = "origin";
+
+        try {
+            RefSpec refSpec = new RefSpec()
+                    .setSource(null)
+                    .setDestination("refs/heads/"+branchName);
+            git.push().setRefSpecs(refSpec).setRemote(remoteServerName).call();
+        } catch (GitAPIException e) {
+            l.error("Failed to delete remote branch "+branchName+" on remote server "+remoteServerName, e);
+            success = false;
+        }
+        return success;
+    }
+
+    public static boolean reset(String ref, ResetCommand.ResetType resetType, Git git){
+        boolean success = true;
+        try {
+            git.reset().setMode( ResetCommand.ResetType.HARD ).setRef( ref ).call();
+        } catch (GitAPIException e) {
+            l.error("Failed to reset ref "+ref, e);
+            success = false;
+        }
+        return success;
     }
 }
